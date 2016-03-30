eb6cc8fe7314bcb93f39b4a86bb922aaee8d2932
Aishani Bhalla <aishani.bhalla0709@gmail.com>
2016-03-29 22:29:51 -0400

    Updated HelloWorld Code

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,15 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
-    }
+       //  I won't work!
+		String version = System.getProperty("java.version");
+		int position = version.indexOf('.');
+		position = version.indexOf('.',position+1);
+		return Double.parseDouble(version.substring(0,position));
+	}
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
-    }
+    	System.out.print(getVersion());
+	}
 }
